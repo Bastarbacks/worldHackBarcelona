e0818fb890a773e28a44181332b933da6ea96737
Oriol Blanc <oriolblanc@gmail.com>
2012-09-18 16:22:05 +0200

    Working on SongInfo

--- iOS/worldHackBacerlona/worldHackBacerlona/Classes/BLL/SongInfoEntity.m
+++ iOS/worldHackBacerlona/worldHackBacerlona/Classes/BLL/SongInfoEntity.m
@@ -9,5 +9,42 @@
 #import "SongInfoEntity.h"
 
 @implementation SongInfoEntity
+@synthesize title = _title;
+@synthesize album = _album;
+@synthesize artist = _artist;
+@synthesize cover = _cover;
+@synthesize preview = _preview;
+
+- (id)initWithTitle:(NSString *)title
+              album:(NSString *)album
+             artist:(NSString *)artist
+              cover:(NSString *)cover
+            preview:(NSString *)preview
+{
+    if (self = [super init])
+    {
+        self.title = title;
+        self.album = album;
+        self.artist = artist;
+        self.cover = cover;
+        self.preview = preview;
+    }
+    
+    return self;
+}
+
+#pragma mark - Memory Management
+
+- (void)dealloc
+{
+    [_title release];
+    [_album release];
+    [_artist release];
+    [_cover release];
+    [_preview release];
+    [_album release];
+    
+    [super dealloc];
+}
 
 @end
