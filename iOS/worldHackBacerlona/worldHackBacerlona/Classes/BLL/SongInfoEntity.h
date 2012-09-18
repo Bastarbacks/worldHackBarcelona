d00da197fe3a1771375b9d12242adc29c0c6df38
Oriol Blanc <oriolblanc@gmail.com>
2012-09-18 16:09:20 +0200

    Added SongInfoEntity to project

---
+++ iOS/worldHackBacerlona/worldHackBacerlona/Classes/BLL/SongInfoEntity.h
@@ -0,0 +1,13 @@
+//
+//  SongInfoEntity.h
+//  worldHackBacerlona
+//
+//  Created by Oriol Blanc on 18/09/12.
+//  Copyright (c) 2012 Bastarbuks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SongInfoEntity : NSObject
+
+@end
---
+++ iOS/worldHackBacerlona/worldHackBacerlona/Classes/BLL/SongInfoEntity.m
@@ -0,0 +1,13 @@
+//
+//  SongInfoEntity.m
+//  worldHackBacerlona
+//
+//  Created by Oriol Blanc on 18/09/12.
+//  Copyright (c) 2012 Bastarbuks. All rights reserved.
+//
+
+#import "SongInfoEntity.h"
+
+@implementation SongInfoEntity
+
+@end
