aeea24df02e4b2535768940fc0b922e265bf9a35
Oriol Blanc <oriolblanc@gmail.com>
2012-09-18 13:55:59 +0200

    Initial project

---
+++ iOS/worldHackBacerlona/worldHackBacerlona/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  worldHackBacerlona
+//
+//  Created by Oriol Blanc on 18/09/12.
+//  Copyright (c) 2012 Bastarbuks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ iOS/worldHackBacerlona/worldHackBacerlona/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  worldHackBacerlona
+//
+//  Created by Oriol Blanc on 18/09/12.
+//  Copyright (c) 2012 Bastarbuks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
