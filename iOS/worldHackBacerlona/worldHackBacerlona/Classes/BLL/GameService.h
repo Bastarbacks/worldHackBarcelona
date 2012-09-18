55f970a795fff941b52400702f14c6bc3ca150d6
Oriol Blanc <oriolblanc@gmail.com>
2012-09-18 14:25:49 +0200

    Added GameService to project

---
+++ iOS/worldHackBacerlona/worldHackBacerlona/Classes/BLL/GameService.h
@@ -0,0 +1,13 @@
+//
+//  GameService.h
+//  worldHackBacerlona
+//
+//  Created by Oriol Blanc on 18/09/12.
+//  Copyright (c) 2012 Bastarbuks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameService : NSObject
+
+@end
---
+++ iOS/worldHackBacerlona/worldHackBacerlona/Classes/BLL/GameService.m
@@ -0,0 +1,13 @@
+//
+//  GameService.m
+//  worldHackBacerlona
+//
+//  Created by Oriol Blanc on 18/09/12.
+//  Copyright (c) 2012 Bastarbuks. All rights reserved.
+//
+
+#import "GameService.h"
+
+@implementation GameService
+
+@end
